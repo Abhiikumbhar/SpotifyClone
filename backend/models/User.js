c40be2db4c011828e2c95fb2721f73925beb5f3d
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-04 00:33:59 +0530

    created User Model
    Created user model with mongoose package

---
+++ backend/models/User.js
@@ -0,0 +1,45 @@
+//req mongoose 
+const mongoose = require("mongoose");
+
+//create a mongoose schema of user
+const User = new mongoose.Schema({
+    firstName:
+    {
+        type:String,
+        require : true,
+    },
+    lastName:
+    {
+        type:String,
+        require : false,
+    },
+    email:{
+        type: String,
+        require : true,
+    },
+    username:{
+        type:String,
+        require:true,
+    },
+    likedSongs:
+    {
+        type : String,
+        default:"",
+    },
+    likedPlaylists:
+    {
+        type : String,
+        default:"",
+    },
+    subscribedArtists:
+    {
+        type : String,
+        default:"",
+    },
+});
+
+//create a model of user 
+const UserModel = mongoose.model("User", User);
+
+//export the user model
+module.exports = UserModel;
