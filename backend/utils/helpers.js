ee0b0eb35ee3fca6d1d117c15373c9d67219509f
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-05 22:56:05 +0530

    register user bug fix

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,9 +1,9 @@
-const jwt = require("jwt");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 exports = {};
 
 exports.getToken = async (email, user) => {
-    const token = jwt.sign({ identifier: user._id });
+    const token = jwt.sign({ identifier: user._id }, process.env.MONGO_PASSWORD);
     return token;
 }
 module.exports = exports;
