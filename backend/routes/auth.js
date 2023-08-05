f9bb5d3e025144cbcfa70c9645780c4e090783e1
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-05 22:32:51 +0530

    register new user
    created a module for registering new user

---
+++ backend/routes/auth.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User")
+const bcrypt = require("bcrypt");
+
+
+router.post("/register", async (req, res) =>{
+    //this code will run when client request for /register API as POST request
+    //req.body will be of format {email, password, firstName, lastName, username}
+
+    const {email, password, firstName, lastName, username} = req.body;
+
+    //find that does the user already exits 
+    const user = await User.findOne({email: email});
+    if(user){
+        return res
+            .status(403)
+            .json({error:"The user is already exist"});
+    }
+
+    //if existing user not found register the new user
+    //first we have to incrypt the password to store securely in the database
+    const hashedPassword = bcrypt.hash(password,10);
+    const newUserData = {email, password : hashedPassword, firstName, lastName,username};
+    const newUser = await User.create(newUserData);
+
+   // we want to create the token to return to user 
+    const token = getToken(email,newUser);
+
+});
