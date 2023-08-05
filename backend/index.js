49ecaca2d9ac1653db9d15b1d01d3ab795a38dc5
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-05 16:18:22 +0530

    Connect DB
    Connect MongoDB database with .env file for password security

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
 const express = require("express"); // to use the expressJS in this file 
+const mongoose = require("mongoose");//req mongoose 
+require("dotenv").config();
 const app = express();      // to get all the functionality of express into a variable named app
 const port = 8000;          // to listen the request form client on port 8000 
+
+//conneting MONGODB with our node app 
+//STORE THE PASSWORD IN .ENV FILE AND THEN USE PROCESS.ENV.PASSWORD TO ACQUIRE THAT PASSWORD
+mongoose
+    .connect("mongodb+srv://abhijeetkofficial:"
+            +process.env.MONGO_PASSWORD
+            +"@spotifydb.gf2kqfk.mongodb.net/?retryWrites=true&w=majority",
+        {
+         useNewUrlParser:true,
+         useUnifiedTopology:true,
+        }
+    )
+    .then((x)=> {
+        console.log("CONNNECTED WITH MONGODB!!!");
+    })
+    .catch((err)=>{
+        console.log("Failed to connect with DB!!");
+    });
 
 app.get("/", (req, res) => {
     // req contains all data for response
