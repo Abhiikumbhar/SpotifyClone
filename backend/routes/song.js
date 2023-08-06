e0512c8ebcb8cefc2a8c8804980c85b8a093c2d7
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-06 23:09:01 +0530

    Update song.js
    create routes to get single song and songs by single artist

--- backend/routes/song.js
+++ backend/routes/song.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Song = require("../models/Song");
+const User = require("../models/User");
+const { route } = require("./song");
 
 router.post(
     "/create",
@@ -28,6 +30,33 @@ router.get(
         const songs = await Song.find({ artist: req.user._id });
         return res.status(200).json({ data: songs });
 
+    }
+);
+
+// route to get all song that any artist has published by the artist id 
+route.get(
+    "/get/artist",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const { artistId } = req.body;
+        // while artist does not exist
+        const artist = await User.find({ _id: artistId });
+        if (!artist) {
+            return res.status(301).json({ err: "Artist does not EXIST!" });
+        }
+        const songs = await Song.find({ artist: artistId });
+        return res.status(200).json({ data: songs });
+    }
+);
+
+//route to get single song by name
+route.get(
+    "/get/name",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const { songName } = req.body;
+        const songs = await Song.find({ name: songName });
+        return res.status(200).json({ data: songs });
     }
 );
 module.exports = router;
