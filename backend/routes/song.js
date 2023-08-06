861449c34f2633664c963c4f6f0c034ceab1fbea
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-06 11:28:34 +0530

    Update song.js
    bug fix

--- backend/routes/song.js
+++ backend/routes/song.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const passport = require("passport");
 const Song = require("../models/Song");
 
-router.post("/create", passport.authenticate(user), async (req, res) => {
-    const { name, thumbnail, track } = req.body;
-    if (!name || !thumbnail || !track) {
-        return res.status(301).json({ err: "Insufficient data" });
+router.post(
+    "/create",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const { name, thumbnail, track } = req.body;
+        if (!name || !thumbnail || !track) {
+            return res.status(301).json({ err: "Insufficient data" });
+        }
+        const artist = req.user._id;
+        const songDetails = { email, thumbnail, track, artist };
+        const createdSong = await Song.create(songDetails);
+        return res.status(200).json(createdSong);
     }
-    const artist = req.user._id;
-    const songDetails = { email, thumbnail, track, artist };
-    const createdSong = await Song.create(songDetails);
-    return res.status(200).json(createdSong);
-});
+);
 
 module.exports = router;
