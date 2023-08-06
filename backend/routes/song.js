c946817db840b9b9fce6632f0bc2e32333eb2faa
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-06 12:46:03 +0530

    Update song.js

--- backend/routes/song.js
+++ backend/routes/song.js
@@ -24,7 +24,6 @@ router.get(
     "/get/mysongs",
     passport.authenticate("jwt", { session: false }),
     async (req, res) => {
-        const currentUser = req.user;
         //we have to get all songs where artist id == currentUser._id
         const songs = await Song.find({ artist: req.user._id });
         return res.status(200).json({ data: songs });
