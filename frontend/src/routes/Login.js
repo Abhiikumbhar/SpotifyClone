96653e1be5eafacb042e6f04068e248b2672f5f4
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-11 23:05:11 +0530

    input format

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,4 +1,6 @@
 import { Icon } from '@iconify/react';
+import TextInput from '../components/shared/TextInput';
+import PasswordInput from '../components/shared/PasswordInput';
 
 const LoginComponent = ()=>{
     return (
@@ -6,9 +8,10 @@ const LoginComponent = ()=>{
             <div className="logo p-4 border-b border-solid border-gray-300 w-full flex justify-center">
                 <Icon icon="logos:spotify" width={150} />
             </div>
-            <div className='inputRegion'>
-                
-
+            <div className='inputRegion w-1/3 py-10 flex item-center justify-center flex-col'>
+                <div className='font-bold mb-10'>to continue, login here</div>
+                <TextInput label="Email address or username" placeholder="Email address or Username"/>
+                <PasswordInput label="Password" placeholder="Password" />
             </div>
             
 
