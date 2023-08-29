f67dda8916c9727ff847390aaa6d75837b446103
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2023-08-29 22:58:15 +0530

    card component
    added card component in the home playlist

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -51,7 +51,7 @@ const Home = () => {
                 <div className="navbar w-full bg-black h-1/10 bg-opacity-40 flex items-center justify-end"> 
                     <div className="w-1/2 h-full flex">
                         <div className="w-3/5 flex justify-around items-center">
-                            <TextWithHover displayText={"Permiun"} />
+                            <TextWithHover displayText={"Premium"} />
                             <TextWithHover displayText={"Support"} />
                             <TextWithHover displayText={"Download"} />
                             <div className="h-1/2 border border-white"></div>
@@ -63,8 +63,8 @@ const Home = () => {
                     </div>
                         
                 </div>
-                <div className="content">
-
+                <div className="content p-8">
+                    <PlaylistView/>
                 </div>
             </div>
         </div>
@@ -72,4 +72,34 @@ const Home = () => {
     )
 };
 
+const PlaylistView = () => {
+    return (
+        <div className="text-white">
+            <div className="text-2xl font-semibold mb-5">Focus</div>
+            <div className="w-full flex justify-between">
+                <Card title={"Peaceful Piano"} description={"Relax and indulge with beautifull piano pieces."}/>
+                <Card title={"Deep Focus"} description={"Keep calm and focus with ambient and post-rock music."}/>
+                <Card title={"Instrumental Study"} description={"Focus with soft study music in the background."}/>
+                <Card title={"Focus Flow"} description={"Uptempo instrumental hip hop beats"}/>
+                <Card title={"Reading Adventure"} description={"Scores and soundtracks for daring quests, epic journeys, and the greatest reading adventures."}/>
+            </div>
+        </div>
+    );    
+}
+
+const Card = ({title,description}) =>{
+    return(
+        <div className="bg-black bg-opacity-60 w-1/6 p-4 rounded-lg">
+            <div className="pb-4 pt-2">
+                <img 
+                    className="w-full rounded-md"
+                    src="https://plus.unsplash.com/premium_photo-1682893682391-9f6c234ddc12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGlhbm98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="thumbnail"
+                />
+            </div>
+            <div className="text-white text-sm font-semibold">{title}</div>
+            <div className="text-gray-500 text-sm">{description}</div>
+        </div>
+    );
+}
 export default Home;
